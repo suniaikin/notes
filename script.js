df2867f8f35130af1bc32ff71248a9ba956baf70
suniaikin <class.suniaikin@gmail.com>
2025-10-22 19:07:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,279 @@
+// Константы
+
+// цвета заметок
+const NOTE_COLORS = { // цвета заметок
+	YELLOW: "yellow",
+	GREEN: "green",
+	BLUE: "blue",
+	PINK: "pink",
+	VIOLET: "violet",
+}
+
+// цвет по умолчанию для новых заметок
+const DEFAULT_COLOR = NOTE_COLORS.YELLOW;
+
+// текстовые константы
+const TEXT_CONSTANTS = {
+	TITLE_INPUT: "Напишите название новой заметки...",
+	TEXT_INPUT: "Напишите текст новой заметки...",
+	ADD_BUTTON: "Добавить",
+	LOGO_TEXT: "NOTES",
+	CALCULATOR: "Всего заметок: ",
+};
+
+// иконки
+const ICONS = {
+	DELETE_BUTTON: "❌",
+	FAVORITE_BUTTON: "🌞",
+	UNFAVORITE_BUTTON: "⛅",
+	LOGO: "📝"
+}
+
+// MODEL
+
+const model = {
+	notes: [],
+
+	deleteNote(id) {
+		this.notes = this.notes.filter(note => note.id !== id);
+	},
+};
+
+// VIEW
+
+const view = {
+
+	//Контенер для шапки
+	headerContainer: document.getElementById("header-container"),
+
+	// Контейнер для формы
+	formContainer: document.getElementById("form-container"),
+
+	// Корневой элемент для рендеринга заметок
+	notesСontainer: document.getElementById("notes-container"),
+
+	// Цвет по умолчанию для новых заметок
+	selectedColor: DEFAULT_COLOR,
+
+
+	//Oтрисовка шапки
+	createHeader() {
+		const appHeader = document.createElement("div");
+		appHeader.classList.add("appHeader");
+
+		const logo = document.createElement("div");
+		logo.classList.add("appHeader-logo");
+		logo.textContent = ICONS.LOGO;
+
+		const title = document.createElement("h1");
+		title.classList.add("appHeader-title");
+		title.textContent = TEXT_CONSTANTS.LOGO_TEXT;
+
+		const notesCount = document.createElement("div");
+		notesCount.classList.add("appHeader-notesCount");
+		notesCount.textContent = `${TEXT_CONSTANTS.CALCULATOR} ${model.notes.length}`;
+		appHeader.append(logo, title, notesCount);
+		return appHeader;
+	},
+
+	updateCounter() {
+		const counter = document.querySelector(".appHeader-notesCount");
+		counter.textContent = `${TEXT_CONSTANTS.CALCULATOR} ${model.notes.length}`
+	},
+
+	// Отрисовка шапки
+	renderHeader() {
+		const headerRender = this.createHeader();
+		this.headerContainer.append(headerRender);
+	},
+
+	// Форма для ввода заметки
+	createForm() {
+		const noteForm = document.createElement("div");
+		noteForm.classList.add("noteForm");
+
+		const titleInput = document.createElement("input");
+		titleInput.type = "text";
+		titleInput.placeholder = TEXT_CONSTANTS.TITLE_INPUT;
+		titleInput.classList.add("noteForm-title");
+
+		const textInput = document.createElement("textarea");
+		textInput.placeholder = TEXT_CONSTANTS.TEXT_INPUT;
+		textInput.classList.add("noteForm-text");
+
+		noteForm.append(titleInput, textInput);
+
+		Object.values(NOTE_COLORS).forEach(color => {
+			const button = document.createElement("button");
+			button.classList.add("note-color");
+			button.classList.add(`note-color--${color}`);
+			noteForm.append(button);
+
+			button.addEventListener("click", () => {
+				this.selectedColor = color;
+				button.classList.add(`note-color-selected`);
+				noteForm.querySelectorAll(".note-color").forEach(btn => {
+					if (btn !== button) {
+						btn.classList.remove("note-color-selected");
+					}
+				});
+			})
+
+		})
+
+		const addButton = document.createElement("button");
+		addButton.textContent = TEXT_CONSTANTS.ADD_BUTTON;
+		addButton.classList.add("noteForm-addButton");
+		noteForm.append(addButton);
+
+		addButton.addEventListener("click", () => {
+			const newNote = {
+				id: Date.now(),
+				title: titleInput.value,
+				text: textInput.value,
+				color: this.selectedColor,
+				isFavorite: false
+			};
+			model.notes.push(newNote);
+			this.renderNotes(model.notes);
+			titleInput.value = "";
+			textInput.value = "";
+			this.updateCounter()
+
+		});
+
+		return noteForm;
+	},
+
+
+	renderForm() {
+		const formRender = this.createForm();
+		this.formContainer.append(formRender);
+	},
+
+
+	// Отрисовка элементов заметки
+	createNoteElement(note) {
+
+		// базовый контейнер заметки
+		const noteElement = document.createElement("div")
+		noteElement.classList.add("noteElement")
+		noteElement.classList.add(`note-color--${note.color}`)
+		noteElement.setAttribute("data-id", note.id)
+
+		// заголовок
+		const titleElement = document.createElement("h3")
+		titleElement.classList.add("noteElement-title");
+		titleElement.textContent = note.title;
+
+		//текст
+		const textElement = document.createElement("p")
+		textElement.textContent = note.text;
+
+		// кнопочный контейнер 
+		const controlsElement = document.createElement("div")
+		controlsElement.classList.add("noteElement-controls");
+
+		// кнопка Удаления
+		const deleteButton = document.createElement("button")
+		deleteButton.classList.add("noteElement-delete");
+		deleteButton.textContent = ICONS.DELETE_BUTTON;
+
+		// кнопка в Избранное
+		const favoriteButton = document.createElement("button")
+		favoriteButton.classList.add("noteElement-favorite");
+		favoriteButton.textContent = note.isFavorite ? ICONS.FAVORITE_BUTTON : ICONS.UNFAVORITE_BUTTON;
+
+		// закинуть в DOM
+		noteElement.append(titleElement, controlsElement, textElement);
+		controlsElement.append(favoriteButton, deleteButton);
+
+		return noteElement;
+	},
+
+	// Функция для рендеринга всех заметок
+	renderNotes(notes) {
+
+		// очистка корневого элемента перед рендерингом
+		this.notesСontainer.innerHTML = "";
+
+		// фрагмент для временного хранения элементов заметок
+		const fragment = document.createDocumentFragment();
+
+		// перебор массива заметок
+		notes.forEach((note) => {
+			const noteElement = this.createNoteElement(note);
+			fragment.append(noteElement);
+		})
+
+		// добавление фрагмента с заметками в корневой элемент
+		this.notesСontainer.append(fragment);
+
+	},
+
+	removeNoteElement(id) {
+		const element = document.querySelector(`[data-id="${id}"]`);
+		if (element !== null) {
+			element.remove()
+		}
+	}
+
+};
+
+// CONTROLLER
+
+const controller = {
+	// Инициализация приложения
+	init() {
+		view.renderHeader()
+		view.renderForm();
+		view.renderNotes(model.notes);
+		this.setupEventListeners();
+	},
+
+	// Прослушка событий
+	setupEventListeners() {
+
+		view.notesСontainer.addEventListener("click", (event) => {
+			const clickedElement = event.target;
+			const noteElement = clickedElement.closest('.noteElement');
+			if (!noteElement) return;
+			if (clickedElement.closest('.noteElement-delete')) {
+				const noteId = Number(noteElement.dataset.id);
+				this.handleDeleteNote(noteId);
+			}
+			if (clickedElement.closest('.noteElement-favorite')) {
+				const noteId = Number(noteElement.dataset.id);
+				this.handleFavoriteNote(noteId);
+			}
+		});
+	},
+
+	handleDeleteNote(id) {
+		model.deleteNote(id);
+		view.removeNoteElement(id)
+		view.updateCounter()
+	},
+
+	handleFavoriteNote(id) {
+		const note = model.notes.find(note => note.id === id);
+		if (note) {
+			note.isFavorite = !note.isFavorite;
+			view.renderNotes(model.notes);
+		}
+	},
+
+};
+
+
+
+
+
+
+
+
+
+
+
+// Инициализация приложения
+controller.init();
